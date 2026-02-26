c15e911be12b4072cc9ea3c3a176b24bf2cbb46e
tim <tim@schmolka.org>
2026-02-26 20:25:24 +0000

    Update SHA for pbak v0.1.0

--- Formula/pbak.rb
+++ Formula/pbak.rb
@@ -2,7 +2,7 @@ class Pbak < Formula
   desc "Photo backup utility: SD card -> SSD -> Immich via immich-go"
   homepage "https://github.com/timschmolka/photo-backup"
   url "https://github.com/timschmolka/photo-backup/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4e03fbcde4a19237d8bd9b870771a386ce390934ca4c019f9d58f770137f98a1"
+  sha256 "00856177bdb04f14544b8665481ac58d817a374f50c9d94a247e6df14bd9a5bc"
   license "MIT"
 
   depends_on "exiftool"
